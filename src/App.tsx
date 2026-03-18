802578d5aab427693158c3e9993ecf5869450ac4
Tako3832 <taytzexuanryann@gmail.com>
2026-03-18 14:39:47 +0800

    fix: add React Router v7 future flags to silence deprecation warnings

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <AuthProvider>
           <ThemeProvider>
             <LanguageProvider>
